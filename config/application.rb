b07ed3315bc33e71e6024cf7b396633a610a895a
John Hu <huziyong@gmail.com>
2014-06-22 00:11:54 +0800

    Remove photos from public

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module GreenifyRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # I18n.default_locale = 'zh-CN'
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
   end
 end
